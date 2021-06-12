e2c3d70a44e13b2c2a27086cb522704844d6caba
Marius Preikschat <m.preikschat@googlemail.com>
2021-06-12 22:30:26 +0200

    Initial Commit

---
+++ Shared/SwiftUI_VideosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_VideosApp.swift
+//  Shared
+//
+//  Created by Marius Preikschat on 12.06.21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_VideosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
