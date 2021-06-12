b576fe94def8c6932b6f43fbabf0ef67315749a0
Marius Preikschat <m.preikschat@googlemail.com>
2021-06-12 22:51:10 +0200

    initial commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(0..<50) { item in
+                Image("90-90")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 90.0)
+                    .cornerRadius(8)
+                Spacer().frame(width: 25)
+                VStack(alignment: .leading) {
+                    Text("The 90 90 Rule is a great rule")
+                        .fontWeight(.semibold)
+                        .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                        .minimumScaleFactor(0.75)
+                    Spacer().frame(height: 5)
+                    Text("20.10.2019")
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                }
+            }.navigationBarTitle(Text("Sean's Videos"))
+        }
     }
 }
 
